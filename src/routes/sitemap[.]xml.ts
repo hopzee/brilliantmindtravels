db512341e946efdffd41cfa28d85680500e3a595
Hopzee <Olowoyeyeopeyemi6@gmail.com>
2026-09-20 10:15:51 +0100

    Update sitemap[.]xml.ts

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,50 +1,37 @@
 import { createFileRoute } from "@tanstack/react-router";
-
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
   <url>
-    <loc>https://brilliantmindtravels.com/</loc>
+    <loc>https://www.brilliantmindtravels.com/</loc>
   </url>
-
   <url>
-    <loc>https://brilliantmindtravels.com/about</loc>
+    <loc>https://www.brilliantmindtravels.com/about</loc>
   </url>
-
   <url>
-    <loc>https://brilliantmindtravels.com/services</loc>
+    <loc>https://www.brilliantmindtravels.com/services</loc>
   </url>
-
   <url>
-    <loc>https://brilliantmindtravels.com/tours</loc>
+    <loc>https://www.brilliantmindtravels.com/tours</loc>
   </url>
-
   <url>
-    <loc>https://brilliantmindtravels.com/blog</loc>
+    <loc>https://www.brilliantmindtravels.com/blog</loc>
   </url>
-
   <url>
-    <loc>https://brilliantmindtravels.com/testimonials</loc>
+    <loc>https://www.brilliantmindtravels.com/testimonials</loc>
   </url>
-
   <url>
-    <loc>https://brilliantmindtravels.com/reviews</loc>
+    <loc>https://www.brilliantmindtravels.com/reviews</loc>
   </url>
-
   <url>
-    <loc>https://brilliantmindtravels.com/faq</loc>
+    <loc>https://www.brilliantmindtravels.com/faq</loc>
   </url>
-
   <url>
-    <loc>https://brilliantmindtravels.com/downloads</loc>
+    <loc>https://www.brilliantmindtravels.com/downloads</loc>
   </url>
-
   <url>
-    <loc>https://brilliantmindtravels.com/contact</loc>
+    <loc>https://www.brilliantmindtravels.com/contact</loc>
   </url>
-
 </urlset>`;
-
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
     handlers: {
